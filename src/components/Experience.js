bfb34a31ede05d3f1ee5dcdc635d89329522f6c8
mayur-kumar <mayur.kumar03@gmail.com>
2020-04-13 21:07:26 +0100

    Experience Page v1

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,15 +1,62 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { makeStyles, Card, Box, Typography, Grid, Paper } from '@material-ui/core'
+import CardContent from '@material-ui/core/CardContent'
+import experience from '../data/ExperienceData'
 
-class Experience extends Component {
-    render() {
-        return(
-            <div>
-                <h1>
-                    Experience
-                </h1>
-            </div>
-        );
-    };
-};
+const useStyles = makeStyles(theme => ({
+    root: {
+        borderRadius: '5',
+        border: '2px solid #fff'
+    },
+    stepper: {
+        orientation: 'vertical'
+    },
+    organisation: {
+        maxWidth: '100%'
+    },
+    projectDescription: {
+        borderLeft: '0.1em solid black',
+        padding: '0.7em'
+    }
+}));
 
-export default Experience
+export default function Experience(){
+    const classes = useStyles();
+
+    return(
+        <Card className={classes.root}>
+            <CardContent>
+                {experience.map(exp => (
+                    <Box pb={6}>
+                        <Typography variant="h4" component="h2">
+                            {exp.organisation}
+                        </Typography>
+                        <Typography variant='body2' color='textSecondary' component='p'>
+                            {exp.startDate} - {exp.endDate}
+                        </Typography>
+                        {exp.projects.map(project => (
+                            <Grid container spacing={2} border={1}>
+                                <Grid item> <Box /> </Grid>
+                                <Grid item xs>
+                                    <Typography gutterBottom variant='h6'>
+                                        {project.designation}
+                                    </Typography>
+                                    <Typography variant='body2' component='p' color='textSecondary'>
+                                        {project.startDate} - {project.endDate}
+                                    </Typography>
+                                    <Typography variant='body2' component='p' color='textSecondary'>
+                                        {project.location}
+                                    </Typography>
+                                    <Typography variant='body2' component='p' className={classes.projectDescription}>
+                                        {project.description}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        ))}
+                    </Box>
+                ))}
+                
+            </CardContent>
+        </Card>
+    )
+}
---
+++ src/data/ExperienceData.js
@@ -0,0 +1,39 @@
+const experience = [
+    {
+        organisation: 'Lloyds Banking',
+        startDate: 'April 2018',
+        endDate: 'now',
+        projects: [
+            {
+                designation: 'DevOps Engineer',
+                startDate: 'Apr 2018',
+                endDate: 'Present',
+                location: 'London, United Kingdom',
+                description: 'Working as a DevOps engineer.'
+            }
+        ]
+    },
+    {
+        organisation: 'Wipro Technologies',
+        startDate: 'Aug 2015',
+        endDate: 'now',
+        projects: [
+            {
+                designation: 'Project Engineer',
+                startDate: 'Aug 2015',
+                endDate: 'March 2018',
+                location: 'Bangalore, India',
+                description: 'Working as a solution architect, for developing solutions around mobile and web testing using various tools.'
+            },
+            {
+                designation: 'Project Engineer',
+                startDate: 'Aug 2015',
+                endDate: 'March 2018',
+                location: 'Bangalore, India',
+                description: 'Worked as Mobile Automation Engineer in Wipro’s Mobility Testing Practice for past 2.5 years. Have an overall experience in testing for 3 years and 7 months in Mobile automation, automation tool development.'
+            },
+        ]
+    }
+]
+
+export default experience
