adf47b3e3eabd48651d5048760f83d67432b56ab
mayur-kumar <mayur.kumar03@gmail.com>
2021-01-25 12:39:25 +0000

    Update experience data

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -1,44 +1,39 @@
 const experience = [
     {
-        organisation: 'Lloyds Banking',
+        organisation: 'Lloyds Banking Group (Wipro)',
         startDate: 'April 2018',
         endDate: 'now',
         projects: [
             {
-                designation: 'DevOps Engineer',
+                designation: 'Senior DevOps Engineer',
                 startDate: 'Apr 2018',
                 endDate: 'Present',
                 location: 'London, United Kingdom',
                 description: [
-                    'Build and maintain CI-CD infrastructure, pipelines and services for dev and test environments.',
                     'Improving and managing service deployments with helm charts.',
-                    'Improved Jenkins deployment with the introduction of JCasC.',
+                    'Instrumented on-premise HA kubernetes cluster, with monitoring support, using Prometheus and Grafana.',
+                    'Improved Jenkins deployment with the introduction of JCasC and helm.',
                     'Written and deployed helm charts using an in-house Kubernetes cluster. Updates to Kubernetes cluster with rke.',
                     'Developed and maintained ansible scripts for provisioning and maintaining macmini build machines as an On-Prem infrastructure.',
                     'Automated and improved workflow with terraform, for github repository, teams, access and webhooks management.',
                     'Migrated the android jenkins pipelines from linux slaves to Kubernetes pods, reducing build times by 50%.',
                     'Deployed and maintained Hashicorp Vault service for secret management.',
                     'Migrating dev/test services to Gitops CD model.',
-                    'Automated pipelines for Software Composition Analysis and SAST scans for android and iOS',
-                    'application security with NexusIQ and Veracode on Jenkins.'
+                    'Automated pipelines for Software Composition Analysis and SAST scans for android and iOS application security with NexusIQ and Veracode on Jenkins.',
+                    'Plan migration of current infrastructure to Google Cloud Platform.'
 
                 ]
             },
             {
-                designation: 'Senior Test Automation Engineer',
-                startDate: 'Aug 2015',
-                endDate: 'March 2018',
-                location: 'Bangalore, India',
+                designation: 'DevOps Engineer',
+                startDate: 'April 2018',
+                endDate: 'Aug 2019',
+                location: 'London',
                 description: [
-                    'Worked as an SME at the Lloyds Banking Group Mobile Application Platform for Test automation.',
                     'Developed a REST API service with ExpressJS for test data management during automated test runs to increase test reliability.',
-                    'Developed a service for user data management on dev environments to aid testing, and deployed with GitlabCI.',
-                    'Developed and maintained the test automation framework written on WDIO.',
-                    'Designed test automation pipelines on Jenkins.',
-                    'Built the open-source tool, for automatically setting up a private device cloud with Selenium Grid and Appium. (Refer Github Links)',
-                    'Automated various manual tasks with Javascript, such as download build files (apk, ipa) from nexus, automated test reports.',
-                    'Successfully completed a technical challenging task of capturing the OTP and authenticating it on a call during the test execution'
-
+                    'Developed and deployed a service for user data management on dev environments to aid testing with GitlabCI.',
+                    'Build and maintain CI-CD infrastructure, pipelines for dev and test environments.',
+                    'Built the open-source tool, for automatically setting up a private device cloud with Selenium Grid and Appium. (Refer Github Links)'
                 ]
             }
         ]
@@ -46,18 +41,16 @@ const experience = [
     {
         organisation: 'Wipro Technologies',
         startDate: 'Aug 2015',
-        endDate: 'now',
+        endDate: 'March 2018',
         projects: [
             {
-                designation: 'Senior Test Consultant',
+                designation: 'Software Developer in Test',
                 startDate: 'Aug 2015',
                 endDate: 'March 2018',
                 location: 'Bangalore, India',
                 description: [
-                    'Developed the platform and tool agnostic test automation framework, XTune, offered as a testing service at Wipro.',
-                    'Contributed to the development of AssureNXT Test Platfrom.',
-                    'Developed testing frameworks and solutions for various clients (GEICO, TD Bank, Kohls)',
-                    'Worked as testing consultant to develop and improve the test automation framework for 8x8.',
+                    'Developed in-house platform and tool agnostic test automation framework (XTune & AssureNXT).',
+                    'Developed testing frameworks and solutions for various clients (GEICO, TD Bank, Kohls, 8x8).',
                     'Worked with Pre-Sales teams, to develop and to build minimal viable products on Request for Proposals (RFP).'
 
                 ]
